525f24a7c36473bd40c58f733696e88af851d9e3
Lindsey Joseph <lindsey.joseph@trustvesta.com>
2020-02-12 16:35:01 -0600

    added suma method to Hello.java

--- src/Hello.java
+++ src/Hello.java
@@ -4,6 +4,11 @@ public class Hello
     public static void main(String args[]) {
         
      System.out.println("Hello word with Java");  
-        
+     System.out.print("El resultado de la suma de 5 + 7 es : "+suma(5,7));
+   
+    }
+    
+   public static int suma (int a, int b) {
+        return a+b; 
     }
 }
